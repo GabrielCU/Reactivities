91dac3872bbb667745ee2f0b7f942541392fcdf6
Gabriel Cordova <gabocordova07@gmail.com>
2023-01-30 23:35:33 -0600

    Implemented Axios crud operations

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,10 +15,11 @@ interface Props{
 	closeForm: () => void
 	createOrEdit: (activity: Activity) => void;
 	deleteActivity: (id: string) => void;
+	submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, 
-		selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+		selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting}: Props) {
 	return(
 		<Grid>
 			<Grid.Column width='10'>
@@ -26,7 +27,8 @@ export default function ActivityDashboard({activities,
 				activities={activities} 
 				selectActivity={selectActivity}
 				deleteActivity={deleteActivity}
-				/>
+				submitting={submitting}
+			/>
 			</Grid.Column>
 			<Grid.Column width='6'>
 				{selectedActivity && !editMode &&
@@ -37,7 +39,12 @@ export default function ActivityDashboard({activities,
 
 				/>}
 				{editMode &&
-				<AcitivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+				<AcitivityForm 
+					closeForm={closeForm} 
+					activity={selectedActivity} 
+					createOrEdit={createOrEdit}
+					submitting={submitting}
+				/>}
 			</Grid.Column>
 		</Grid>
 	)
